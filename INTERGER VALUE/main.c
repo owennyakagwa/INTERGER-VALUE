22ca4de4c1e7e783d427396c1360b00c34bb3b81
owennyakagwa <97963120+owennyakagwa@users.noreply.github.com>
2023-06-04 14:45:59 +0300

    Add files via upload

---
+++ INTERGER VALUE/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter an integer value: ");
+    scanf("%d", &number);
+
+    printf("The entered value is: %d\n", number);
+    return 0;
+}
